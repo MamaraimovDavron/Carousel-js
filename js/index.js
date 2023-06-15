0d460594dc143e5e4abb6406f5be50a150d66ff9
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-06-15 11:30:15 +0500

    Carousel done

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const slide = document.getElementById('slide');
+const previousButton = document.getElementsByClassName('carousel-control-prev');
+const nextButton = document.getElementsByClassName('carousel-control-next');
+const imgs = [
+    '../imgs/img1.jpg',
+    '../imgs/img2.jpg',
+    '../imgs/img3.jpg'
+];
+
+let active = 0;
+
+function toLeft(){
+    active--;
+    if(active < 0) active = imgs.length - 1;
+    slide.src = imgs[active];
+}
+
+function toRight(){
+    active++;
+    if(active >= imgs.length) active = 0;
+    slide.src = imgs[active];
+}
