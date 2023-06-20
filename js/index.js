a12db85c8ed5c2121e9ad4d6cebcba7da8387e99
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-06-20 11:39:41 +0500

    btns added

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
-const slide = document.getElementById('slide');
-const previousButton = document.getElementsByClassName('carousel-control-prev');
-const nextButton = document.getElementsByClassName('carousel-control-next');
+// const slide = document.getElementById('slide');
+// const previousButton = document.getElementsByClassName('carousel-control-prev');
+// const nextButton = document.getElementsByClassName('carousel-control-next');
+// // const imgs = ['../imgs/img1.jpg','../imgs/img2.jpg','../imgs/img3.jpg'];
 // const imgs = ['../imgs/img1.jpg','../imgs/img2.jpg','../imgs/img3.jpg'];
-const imgs = ['../imgs/img1.jpg','../imgs/img2.jpg','../imgs/img3.jpg'];
-let active = 0;
+// let active = 0;
 
-function toLeft(){
-    active--;
-    if(active < 0) active = imgs.length - 1;
-    slide.src = imgs[active];
-    document.getElementById('number').innerHTML = active + 1;
-    console.log('to left');
-}
+// function toLeft(){
+//     active--;
+//     if(active < 0) active = imgs.length - 1;
+//     slide.src = imgs[active];
+//     document.getElementById('number').innerHTML = active + 1;
+//     console.log('to left');
+// }
 
-function toRight(){
-    active++;
-    if(active >= imgs.length) active = 0;
-    slide.src = imgs[active];
-    document.getElementById('number').innerHTML = active + 1;
-    console.log('to right');
-}
+// function toRight(){
+//     active++;
+//     if(active >= imgs.length) active = 0;
+//     slide.src = imgs[active];
+//     document.getElementById('number').innerHTML = active + 1;
+//     console.log('to right');
+// }
