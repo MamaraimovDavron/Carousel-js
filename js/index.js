fdd9f14c365bf90c17aa5a278fc18f7dd911c37c
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-06-20 13:54:19 +0500

    Again redone

--- js/index.js
+++ js/index.js
@@ -1,30 +1,30 @@
-// const slide = document.getElementById('slide');
-// const previousButton = document.getElementsByClassName('carousel-control-prev');
-// const nextButton = document.getElementsByClassName('carousel-control-next');
-// // const imgs = ['../imgs/img1.jpg','../imgs/img2.jpg','../imgs/img3.jpg'];
+const slide = document.getElementById('slide');
+const previousButton = document.getElementsByClassName('carousel-control-prev');
+const nextButton = document.getElementsByClassName('carousel-control-next');
 // const imgs = ['../imgs/img1.jpg','../imgs/img2.jpg','../imgs/img3.jpg'];
-// let active = 0;
+const imgs = ['./imgs/img1.jpg','./imgs/img2.jpg','./imgs/img3.jpg'];
+let active = 0;
 
-// function toLeft(){
-//     active--;
-//     if(active < 0) active = imgs.length - 1;
-//     slide.src = imgs[active];
-//     document.getElementById('number').innerHTML = active + 1;
-//     console.log('to left');
-// }
+function toLeft(){
+    active--;
+    if(active < 0) active = imgs.length - 1;
+    slide.src = imgs[active];
+    document.getElementById('number').innerHTML = active + 1;
+    console.log('to left');
+}
 
-// function toRight(){
-//     active++;
-//     if(active >= imgs.length) active = 0;
-//     slide.src = imgs[active];
-//     document.getElementById('number').innerHTML = active + 1;
-//     console.log('to right');
-// }
+function toRight(){
+    active++;
+    if(active >= imgs.length) active = 0;
+    slide.src = imgs[active];
+    document.getElementById('number').innerHTML = active + 1;
+    console.log('to right');
+}
 
 const btnOne = document.getElementById('btn-one');
 const btnTwo = document.getElementById('btn-two');
 const btnThree = document.getElementById('btn-three');
-const imgs = ['./imgs/img1.jpg','./imgs/img2.jpg','./imgs/img3.jpg'];
+// const imgs = ['./imgs/img1.jpg','./imgs/img2.jpg','./imgs/img3.jpg'];
 
 
 function clicked1() {
